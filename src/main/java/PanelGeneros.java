23211909f7ed1389e99a74aab6b1f85ee42a04b2
Samuel <mimail@gmail.com>
2021-05-26 17:22:33 +0200

    JTextfield listener mal.

--- src/main/java/PanelGeneros.java
+++ src/main/java/PanelGeneros.java
@@ -1,13 +1,26 @@
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-public class PanelGeneros {
+public class PanelGeneros implements DocumentListener{
     private static JPanel panel;
     private static JButton[] botones;
-    public static JPanel panelgen() {
-        panel = new JPanel(new GridLayout(0, 1));
+
+
+    public  JPanel panelgen() {
+        GridLayout layout = new GridLayout(0, 1);
+        panel = new JPanel(layout);
         botones = new JButton[Generos.values().length];
+        //layout.setHgap(10);
+        layout.setVgap(10);
+        JTextField textField = new JTextField("gfr");
+        panel.add(textField);
+        JLabel label = new JLabel();
+        panel.add(label);
+        textField.getDocument().addDocumentListener(this);
+
 
         for (Generos g : Generos.values()) {
             int contador = 0;
@@ -19,6 +32,21 @@ public class PanelGeneros {
     }
 
     private static void accion() {
+
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent documentEvent) {
+        System.out.println("Texto insertado");
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent documentEvent) {
+
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent documentEvent) {
 
     }
 }
--- src/main/java/Ventana.java
+++ src/main/java/Ventana.java
@@ -1,14 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Ventana extends JFrame {
+    PanelGeneros panelGeneros = new PanelGeneros();
 
     public void programa() {
-        this.add(PanelGeneros.panelgen());
-
+        this.add(panelGeneros.panelgen(), BorderLayout.WEST);
         this.setTitle("VDCB");
+        this.setSize(1280,720);
         this.setLocationRelativeTo(null);
-        this.pack();
-        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //this.pack();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
 }
