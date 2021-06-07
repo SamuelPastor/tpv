d5a0ebdecb9d96611d6dcbdbe0bcfe881e8d2797
SamuelPastor <samupastor23@gmail.com>
2021-06-07 15:46:29 +0200

    1 peli

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,8 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        //ImageIcon imagen = new ImageIcon(".\\imagenes\\Rocky.jpg");
-        //ESPelicula.anyadirPelicula(new Pelicula("El Rocky", "175", 8, 24, imagen, Generos.ACCION));
-        //ESPelicula.leerPeliculas();
         Ventana ventana = new Ventana();
         ventana.programa();
-        //ESPelicula.leerPeliculas();
 
     }
 }
